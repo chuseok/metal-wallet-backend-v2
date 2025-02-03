fe80800afe490fd8eb92a095a0710c4c3ac95343
chuseok <90127lucy@gmail.com>
2025-02-03 11:11:17 +0900

    refactor: `Member` length = 11 -> 12로 변경

--- src/main/java/com/kb/wallet/member/domain/Member.java
+++ src/main/java/com/kb/wallet/member/domain/Member.java
@@ -42,7 +42,7 @@ public class Member implements UserDetails {
   @Column(nullable = false, length = 10)
   private String name;
 
-  @Column(unique = true, length = 11)
+  @Column(unique = true, length = 12)
   private String phone;
 
   @Column(nullable = false)
