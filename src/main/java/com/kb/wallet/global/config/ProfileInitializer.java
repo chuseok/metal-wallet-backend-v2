4a78234cf3752dc50f98d13298f62fd84b520f70
chuseok <90127lucy@gmail.com>
2025-11-27 13:03:33 +0900

    chore: profile 활성화 로그 확인 - logback.xml 오타 수정

--- src/main/java/com/kb/wallet/global/config/ProfileInitializer.java
+++ src/main/java/com/kb/wallet/global/config/ProfileInitializer.java
@@ -1,13 +1,20 @@
 package com.kb.wallet.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 public class ProfileInitializer implements
     ApplicationContextInitializer<ConfigurableApplicationContext> {
 
   @Override
   public void initialize(ConfigurableApplicationContext applicationContext) {
     applicationContext.getEnvironment().setActiveProfiles("prod");  // refresh 전에 적용됨
+
+
+    String[] activeProfiles = applicationContext.getEnvironment().getActiveProfiles();
+    log.info("Root WebApplicationContext: initialization started");
+    log.info("Active profiles: {}", String.join(", ", activeProfiles));
   }
 }
