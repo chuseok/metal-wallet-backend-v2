c6365118fcd78adbe9884754b96465939877750f
chuseok <90127lucy@gmail.com>
2025-12-08 16:25:31 +0900

    feat: JVM metric 바인딩 추가

--- src/main/java/com/kb/wallet/global/config/MetricsConfig.java
+++ src/main/java/com/kb/wallet/global/config/MetricsConfig.java
@@ -1,14 +1,35 @@
 package com.kb.wallet.global.config;
 
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricsConfig {
+  private final PrometheusMeterRegistry registry;
+
+  public MetricsConfig(PrometheusMeterRegistry registry) {
+    this.registry = registry;
+  }
 
   @Bean
   public PrometheusMeterRegistry prometheusMeterRegistry() {
     return new PrometheusMeterRegistry(io.micrometer.prometheus.PrometheusConfig.DEFAULT);
+  }
+
+  @PostConstruct
+  public void bindMetrics() {
+    new ClassLoaderMetrics().bindTo(registry);
+    new JvmMemoryMetrics().bindTo(registry);
+    new JvmGcMetrics().bindTo(registry);
+    new ProcessorMetrics().bindTo(registry);
+    new UptimeMetrics().bindTo(registry);
   }
 }
