6897dd88d721b1032db9c9c26395fb29327ae8bf
chuseok <90127lucy@gmail.com>
2025-12-31 17:24:52 +0900

    fix: timer 재사용

--- src/main/java/com/kb/wallet/global/metrics/HttpMetricsFilter.java
+++ src/main/java/com/kb/wallet/global/metrics/HttpMetricsFilter.java
@@ -3,6 +3,7 @@ package com.kb.wallet.global.metrics;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
 import java.io.IOException;
+import java.time.Duration;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import lombok.extern.slf4j.Slf4j;
@@ -28,22 +29,29 @@ public class HttpMetricsFilter implements Filter {
     }
 
     HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
 
     String path = normalizeUri(httpRequest.getRequestURI());
     String method = httpRequest.getMethod();
+    String status = String.valueOf(httpResponse.getStatus());
 
     Timer.Sample sample = Timer.start(registry);
 
     try {
       chain.doFilter(request, response);
     } finally {
-      Timer timer = Timer.builder("http.server.requests")//http_server_requests_count
+      Timer timer = Timer.builder("http.server.requests")
           .description("HTTP Server Requests")
-          .tags("uri", path, "method", method)
-          .publishPercentileHistogram(true)
+          .tags("uri", path, "method", method, "status", status)
+          .publishPercentileHistogram()
+          .serviceLevelObjectives(
+              Duration.ofMillis(100),
+              Duration.ofMillis(300),
+              Duration.ofMillis(500),
+              Duration.ofSeconds(1),
+              Duration.ofSeconds(3)
+          )
           .register(registry);
-
-      sample.stop(timer);
     }
   }
 
